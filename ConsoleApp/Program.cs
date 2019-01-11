d66d40f39d9901cded2a11ad6aade02ab1ac3af1
Jensen Somers <jensen.somers@macadam.eu>
2019-01-11 13:29:08 +0100

    add sample app

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Library;
+
+namespace ConsoleApp
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            HelloWorld.Print();
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Library/HelloWorld.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Library
+{
+    public static class HelloWorld
+    {
+        public static void Print() => Console.WriteLine("Hello World!");
+    }
+}
